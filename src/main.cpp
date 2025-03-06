a87f123d136fd15ce75e8ab790c6b446ca1a7155
Navarro <ricardo.navarro@harman.com>
2025-03-06 13:52:21 -0600

    Add Comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,38 @@
+/**
+ * The code sends a message to a phone number using the CallMeBot API.
+ * The message is sent using the WhatsApp service.
+ * 
+ * Author:  @RikuNav
+ */
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <WiFiClientSecure.h>
 #include <UrlEncode.h>
 
-const char* ssid = "XXXXXXXXXXX";
-const char* password = "XXXXXX";
+// WiFi credentials
+const char* ssid = "XXXXXXXXX"; // The SSID of the internet
+const char* password = "XXXXXX"; // The password of internet
 
 // Number Strings
-String phoneNumber = "XXXXXXXXXXXXX";
-String apiKey = "XXXXXX";
+String phoneNumber = "XXXXXXXXXXX"; // The phone number displayed by the API
+String apiKey = "XXXXXXX"; // The API key (DO NOT SHARE IT!!!!)
+
+// Function prototypes
+void connectToWiFi();
+void sendMessage(String);
+
+void setup() {
+  connectToWiFi();
+  sendMessage("Hello from ESP8266!");
+}
+
+void loop() {
+}
 
 void connectToWiFi() {
+  /**
+   * Connects to the WiFi network
+  */
   Serial.begin(115200);
   delay(10);
   pinMode(LED_BUILTIN, OUTPUT);
@@ -37,17 +58,13 @@ void connectToWiFi() {
   digitalWrite(LED_BUILTIN, LOW); // Turn the LED on (LOW is the voltage level)
 }
 
-void sendMessage(String);
-
-void setup() {
-  connectToWiFi();
-  sendMessage("Hello from ESP8266!");
-}
-
-void loop() {
-}
 
 void sendMessage(String message) {
+  /**
+   * Sends a message to the phone number using the CallMeBot API
+   * 
+   * @param message The message to send
+  */
   String url = "https://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&text=" + urlEncode(message) + "&apikey=" + apiKey;
 
   WiFiClientSecure client;
